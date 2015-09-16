ae58b8dd82667bab0a71741dfb27bdfd740a3b89
Casey Heitz & Sam Martinez <student@epicodus.com>
2015-09-16 09:44:12 -0700

    add testing and pass for space and player

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 function Player(mark) {
   this.mark = mark;
 }
+
+function Space(xCoordinate, yCoordinate, markedBy) {
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+  this.markedBy = markedBy;
+}
+
+Space.prototype.mark = function(player) {
+  this.markedBy = player;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,18 @@ describe('Player', function() {
     expect(testPlayer.mark).to.equal("X");
   });
 });
+
+describe('Space', function() {
+  it("returns the correct coordinates of a space", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.xCoordinate).to.equal(1);
+    expect(testSpace.yCoordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player("X");
+    var testSpace = new Space(1,2);
+    testSpace.mark(testPlayer);
+    expect(testSpace.markedBy).to.equal(testPlayer);
+  });
+});
